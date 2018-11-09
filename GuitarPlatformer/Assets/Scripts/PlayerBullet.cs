475cb6ef1f5741fbd9faa0c269a42eaa42b6c62f
Coehl Gleckner (RIT Student) <crg5646@ad.rit.edu>
2018-11-09 17:14:17 -0500

    Got the ignore working again

---
+++ GuitarPlatformer/Assets/Scripts/PlayerBullet.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBullet : MonoBehaviour {
+
+    // attributes
+    GameObject player;
+    public float speed = 4.0f;
+
+	// Use this for initialization
+	void Start ()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(speed, 0);
+        player = GameObject.FindGameObjectWithTag("Player");
+        Physics2D.IgnoreCollision(GetComponent<Collider2D>(), player.GetComponent<Collider2D>());
+        GetComponent<SpriteRenderer>().color = GetRandomColor();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		if (player == null)
+            player = GameObject.FindGameObjectWithTag("Player");
+
+        if (Vector2.Distance(transform.position, player.transform.position) > 25f)
+            Destroy(gameObject);
+    }
+
+    // Gets a random color
+    Color GetRandomColor() { return new Color(Random.Range(0, 255) / 255.0f, Random.Range(0, 255) / 255.0f, Random.Range(0, 255) / 255.0f); }
+}
