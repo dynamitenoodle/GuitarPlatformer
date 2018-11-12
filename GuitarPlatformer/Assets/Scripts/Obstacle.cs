2c4bf66d671458e23a680d77de8d600782d68341
Storm <stormmayer@gmail.com>
2018-11-12 14:36:29 -0500

    Prefab + Layout
    I created an obstacle prefab and began a layout for the obstacle script. I'm not able to use teh Unity data types on my laptop, so I won't puch to master until I'm able to get some real work done on it.

---
+++ GuitarPlatformer/Assets/Scripts/Obstacle.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour {
+
+    public bool destructable;
+    public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+        destructable = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Move();
+        CheckCollision();
+        DestructableCollision();
+        Cleanup();
+	}
+
+    //Shift the obstacle with the background
+    void Move()
+    {
+        //constantly sliding left until cleanup
+    }
+
+    //Delete obstacle upon leaving the screen
+    void Cleanup()
+    {
+        //delete at x -7.3
+    }
+
+    //Collision Check
+    void CheckCollision()
+    {
+        //this will kill the player if they aren't shielded
+    }
+
+    //Collisions if Destructable
+    void DestructableCollision()
+    {
+        if (destructable)
+        {
+            //check collisions with shielded or projectile
+
+        }
+    }
+}
